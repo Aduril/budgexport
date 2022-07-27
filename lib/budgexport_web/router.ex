edf01fd128bd53f8b42d1b27d4c991275e2e1f98
Peter Melinat <petermelint@MBP-von-Peter.fritz.box>
2022-07-27 23:24:48 +0200

    testing something

--- lib/budgexport_web/router.ex
+++ lib/budgexport_web/router.ex
@@ -18,6 +18,7 @@ defmodule BudgexportWeb.Router do
     pipe_through :browser
 
     live "/", BudgetLive
+    live "/thisisjustatestforme", BudgetLive
   end
 
   # Other scopes may use custom stacks.
